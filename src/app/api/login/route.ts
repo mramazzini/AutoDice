863108d2caab1767c50eb93007cd4762ce5d52b8
mramazzini <mramazzini123@gmail.com>
2024-01-24 15:55:11 -0600

    login GET

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,5 +1,15 @@
 "use server";
 
-export async function GET() {
-  return Response.json({ name: "John Doe" });
+import login from "@/app/actions/login";
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const email = searchParams.get("email") || "";
+  const password = searchParams.get("password") || "";
+  try {
+    const res = await login({ email, password });
+    return Response.json(res);
+  } catch (err: any) {
+    return Response.json({ error: err.message });
+  }
 }
