b443ba8af7f9facaa2db9aa77e91fb8502c6163f
mramazzini <mramazzini123@gmail.com>
2024-01-24 13:11:25 -0600

    fixed next project structure

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,10 @@
 import LoginForm from "./LoginForm";
 
-export default function LoginPage() {
+export default async function LoginPage() {
   return (
     <div>
       <h1>Login</h1>
+
       <LoginForm />
     </div>
   );
---
+++ src/pages/signup/SignupForm.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { useState } from "react";
+import { PrismaClient } from "@prisma/client";
+export default function SignupForm() {
+  const [formState, setFormState] = useState({
+    username: "", // or email
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormState({
+      ...formState,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setError("");
+    fetch("/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formState),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={formState.username}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          value={formState.password}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+      {error && <p>{error}</p>}
+    </form>
+  );
+}
